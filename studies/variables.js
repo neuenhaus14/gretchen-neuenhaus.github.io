051b3cae9746442e78c043754f18a354fb7c6cde
Gretchen Neuenhaus <neuenhaus14@gmail.com>
2023-06-24 20:25:33 +0000

    description

---
+++ studies/variables.js
@@ -0,0 +1,35 @@
+/** 
+ * VARIABLES
+ * 0: essential and important info
+ * 5 sentences is fine
+ * a nice summary
+ * Variables are name containers for data in 
+ * javascript. They can be assigned to any data type
+ * by declarative names. 
+ * 
+ * 
+ * 1: Declaration and Assignment: describe whats going on
+ * To create a varible in javascript we must first decalre it. 
+ * We do that by using one of 3 declarative key words. 
+ *  * 
+ * 
+ * 2: Var, let, const: These are the three declarative keywords. 
+ * 
+ * 3: Hoisting: describe hoisting
+ 
+ * */
+
+// 1. declaration and assignment //
+// declaration- this needs to be explained as we are coding
+var age;
+
+// assignment
+// age is now assigned to the number 27 as it's value
+age = 27;
+
+// 2. var let and const
+
+// for operators, he needs to see EVERY EXAMPLE
+// string manipulation, HE NEEDS TO SEE AT LEAST 4 METHODS
+
+
