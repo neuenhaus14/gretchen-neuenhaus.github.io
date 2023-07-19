7046ccf767f264cf98a43adf835be95982986a3f
Gretchen Neuenhaus <neuenhaus14@gmail.com>
2023-07-19 21:02:36 +0000

    description

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -103,7 +103,7 @@ return listToArray(list.rest, output);
 function prepend(num, list) { //Then add a helper function prepend, which takes an element and 
   //a list and creates a new list that adds the element to the front of the input list
 
-list = {value: num, rest: {list} }
+return {value: num, rest: list}
 
 }
 
